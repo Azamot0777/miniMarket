0fd779dd849bc1692a800baf30f4ea4f857bf1e5
Heisenberg <Persey_98@list.ru>
2020-03-28 19:09:58 +0500

    Lesson

--- Program.cs
+++ Program.cs
@@ -1,132 +1,93 @@
 ﻿using System;
-namespace lesson3
+
+namespace MyConsoleApp
 {
-    public class Programm
+    class  Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            // System.Console.WriteLine("Число:");
+            // int number = int.Parse (Console.ReadLine());
+            // for(int i = 0; i <= 10; i++)
+            // {
+            //     System.Console.WriteLine($"{number} X {i} = {number * i}");
+            // }
 
-            decimal userBalance = 300m;
-            string basket = "";
-            string erroBalanceMessage = "У вас недостаточно средств";
 
-        start: //Метка "start"
 
-            System.Console.WriteLine($"Ваш баланс - {userBalance}");
+            // System.Console.WriteLine("Найта нечетные числа от - до");
+            // System.Console.WriteLine("Oт Числа:");
+            // int beginNumber = int.Parse (Console.ReadLine());
 
-            if (userBalance <= 0)
-            {
-                System.Console.WriteLine(erroBalanceMessage);
-                goto End;
-            };
+            // System.Console.Write("До Числа:");
+            // int toNumber = int.Parse (System.Console.ReadLine());
+            // for (int i = beginNumber; i <= toNumber; i++)
+            // {
+            //     if (i % 2 == 0)
+            //     {
+            //         System.Console.WriteLine(i);
+            //     }
+            // }
 
-            System.Console.WriteLine("" +
-            "1 Кола - 10 сомон\n" +
-            "2 Сникерс - 7 сомон\n" +
-            "3 Марс - 9 сомон\n" +
-            "4 M&Ms - 4 сомон\n" +
-            "5 Чипсы - 14 сомон\n" +
-            "6 Выход\n" +
-            "Ваш выбор (1-6):");
 
-            int choice = int.Parse(Console.ReadLine());
+            
+            //System.Console.WriteLine(DateTime.Now.Month);
 
-            // goto start; //идти к метке "start"
-            switch (choice)
-            {
-                case 1:
-                    {
-                        if (userBalance - 10 >= 0)
-                        {
-                            basket += "Кола,";
-                            userBalance -= 10;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    break;
-                case 2:
-                    {
-                        if (userBalance - 7 >= 0)
-                        {
-                            basket += "Cникерс,";
-                            userBalance -= 7;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    break;
-                case 3:
-                    {
-                        if (userBalance - 9 >= 0)
-                        {
-                            basket += "Марс,";
-                            userBalance -= 9;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    break;
-                case 4:
-                    {
-                        if (userBalance - 4 >= 0)
-                        {
-                            basket += "M&Ms,";
-                            userBalance -= 4;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    break;
-                case 5:
-                    {
-                        if (userBalance - 14 >= 0)
-                        {
-                            basket += "Чипсы,";
-                            userBalance -= 14;
-                        }
-                        else
-                        {
-                            System.Console.WriteLine(erroBalanceMessage);
-                            goto End;
-                        }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
-                        if (Console.ReadLine().ToLower() == "д") goto start;
-                        else goto End;
-                    }
-                    break;
-                case 6: goto End;
-                default:
-                    goto start;
-                    break;
-            }
 
-        End:
-            System.Console.WriteLine($"Ваши покупки:{basket}\nВаш баланс:{userBalance}");
+            // DateTime dateTimeAfter5Second = DateTime.Now.AddSeconds (5);
+            // for (;;)
+            // {
+            //     Console.Clear();
+            //     System.Console.WriteLine(DateTime.Now);
+            //     if (dateTimeAfter5Second.ToString("dd/MM/yyyy hh:mm:ss") == DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"))
+            //     {
+            //         break;
+            //     }
+            // }
+
+
+            // int iForWhile = 0;
+            // while (iForWhile < 10)
+            // {
+            //     System.Console.WriteLine(iForWhile);
+            //     iForWhile++;
+            // }
+
+
+
+            // int number = 5;
+            // int numberOfTry = 0;
+
+
+            // while (numberOfTry != 0) 
+            // {
+            //     System.Console.WriteLine("Отгадай число от 1 - 10:");
+            //     int inputNumber = int.Parse (Console.ReadLine());
+            //     if (inputNumber != number)
+            //     {
+            //         numberOfTry--;
+            //         System.Console.WriteLine($"Попыток осталось: {numberOfTry}");
+            //     }
+            //     else 
+            //     {
+            //         System.Console.WriteLine("Поздравляю ты угадал");
+            //         break;
+            //     }
+            // }
+
+
+
+            // int amount = 5;
+            // do 
+            // {
+            //     System.Console.WriteLine(amount);
+            //     amount--;
+            // }
+            // while (amount > 0);
+
+
+            
+
             Console.ReadKey();
         }
     }
